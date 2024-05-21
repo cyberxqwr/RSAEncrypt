8e1a0f7a75fe88a648574b96470c49bb12de7598
Emilis Borusas <134013882+cyberxqwr@users.noreply.github.com>
2024-05-21 04:38:37 +0300

    SocketServer for DigitalSignature

---
+++ SocketServer/SocketServer/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            
+
+            IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+            Program listener = new Program();
+            await listener.Listener(ipAddress, 11_000);
+            
+
+        }
+
+        private async Task Listener(IPAddress ipAddress, int port)
+        {
+
+            DataHolder dataHolder = new DataHolder();
+
+            IPEndPoint ipEndPoint = new(ipAddress, port);
+
+            using Socket listener = new(
+                ipEndPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            listener.Bind(ipEndPoint);
+            listener.Listen(100);
+
+            var handler = await listener.AcceptAsync();
+            while (true)
+            {
+                // Receive message.
+                var buffer = new byte[1_024];
+                var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+                var eom = "<|EOM|>";
+                if (response.IndexOf(eom) > -1 /* is end of message */)
+                {
+                    Console.WriteLine(
+                        $"Socket server received message: \"{response.Replace(eom, "")}\"");
+
+                    dataHolder.body = response;
+
+                    var ackMessage = "Pranesimas gautas";
+                    var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+                    await handler.SendAsync(echoBytes, 0);
+                    Console.WriteLine(
+                        $"Socket server sent acknowledgment: \"{ackMessage}\"");
+
+                    break;
+                }
+                // Sample output:
+                //    Socket server received message: "Hi friends 👋!"
+                //    Socket server sent acknowledgment: "<|ACK|>"
+            }
+
+            listener.Dispose();
+
+            Program program = new Program();
+
+            await program.Sender(ipAddress, 11_001, dataHolder.body);
+        }
+
+        private async Task Sender(IPAddress ipAddress, int port, string body)
+        {
+
+            IPEndPoint ipEndPoint = new(ipAddress, port);
+
+            using Socket sender = new(
+                ipEndPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            sender.Bind(ipEndPoint);
+            sender.Listen(100);
+
+            var handler = await sender.AcceptAsync();
+            while (true)
+            {
+                // Receive message.
+                var buffer = new byte[1_024];
+                var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+                var eom = "<|EOM|>";
+                if (response.IndexOf(eom) > -1 /* is end of message */)
+                {
+                    Console.WriteLine(
+                        $"Socket server received message: \"{response.Replace(eom, "")}\"");
+
+                    var ackMessage = body;
+                    var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+                    await handler.SendAsync(echoBytes, 0);
+                    Console.WriteLine(
+                        $"Socket server sent acknowledgment: \"{ackMessage}\"");
+
+                    break;
+                }
+                // Sample output:
+                //    Socket server received message: "Hi friends 👋!"
+                //    Socket server sent acknowledgment: "<|ACK|>"
+            }
+
+            Console.ReadKey();
+        }
+
+        private class DataHolder
+        {
+
+            public string body;
+        }
+    }
+
+    
+}
